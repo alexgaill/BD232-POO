900f9cc412931d27b81e62e47e4ed853fca3ceaf
Alexandre Gaillot <contact@steptosuccess.fr>
2022-11-02 11:31:03 +0100

    Ajout CategoryManager

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Fixtures\AppFixtures;
-
+use App\Manager\CategoryManager;
 
 define("ROOT", __DIR__);
 require ROOT. "/vendor/autoload.php";
 
-(new AppFixtures)->load();
-echo "Les fixtures sont chargées";
+// Charge de fausses données en BDD pour les tests
+// (new AppFixtures)->load();
+// echo "Les fixtures sont chargées";
 
+// Affiche notre page de catégories
+(new CategoryManager)->getCategories();
 
 
 
